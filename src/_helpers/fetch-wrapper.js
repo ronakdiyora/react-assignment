c052c2381066c62f2763874c905b5e8c667a07ae
Ronak Diyora <ronakdiyora@outlook.com>
2023-03-27 00:47:05 +0530

    integrate login api

--- src/_helpers/fetch-wrapper.js
+++ src/_helpers/fetch-wrapper.js
@@ -36,7 +36,7 @@ function authHeader(url) {
 }
 
 function authToken() {
-    return store.getState().auth.user?.token;
+    return store.getState().auth.user?.tokens.access.token;
 }
 
 function handleResponse(response) {
